76af63fc4bf2e05e37dc068f39022ed19e42153d
lianran <lianran@yeah.net>
2016-12-21 14:00:57 +0800

    .

--- newone/create.sh
+++ newone/create.sh
@@ -9,15 +9,17 @@ echo "every tx will wait $waittime s"
 echo "there will have $round rounds"
 echo "the key for this test is $key"
 unixtime=$(date +%s)
+id=0
 date
-CC_ID="mycc"
+CC_ID="db4fa76606a419673dfc30c929b3950f37c9525ce9739d510b4d4e6529a1986e898435ce5cb36725dd48eb9b48fa8d9b5b18417070c1079db9c8045698ff760c"
 for ((i=0; i < $round; i++)); do
     echo "round $i"
     for ((j=0; j < $num; j++)); do
         #peer chaincode invoke -n ${CC_ID} -c "{\"Args\": [\"create\", \"cp_${i}_${key}_${j}\",\"JD\", \"PKU\", \"${unixtime}\", \"price:${i}${j}\"]}"  -u ${user} &
-        curl -X POST -H 'Content-Type:application/json' --data "{\"jsonrpc\":\"2.0\",\"method\":\"invoke\",\"params\":{\"type\":1,\"chaincodeID\":{\"name\":\"${CC_ID}\"},\"ctorMsg\":{\"args\":[\"create\",\"cp_${i}_${key}_${j}\",\"JD\",\"PKU\",\"${unixtime}\",\"price:${i}${j}\"]},\"secureContext\":\"${user}\"},\"id\":1}" localhost:7050/chaincode
+        curl -X POST -H 'Content-Type:application/json' --data "{\"jsonrpc\":\"2.0\",\"method\":\"invoke\",\"params\":{\"type\":1,\"chaincodeID\":{\"name\":\"${CC_ID}\"},\"ctorMsg\":{\"args\":[\"create\",\"cp_${i}_${key}_${j}\",\"JD\",\"PKU\",\"${unixtime}\",\"price:${i}${j}\"]},\"secureContext\":\"${user}\"},\"id\":${id}}" localhost:7050/chaincode
         #echo "the key is : ${key}${i}${j}"
     #sleep $waittime
+    id++
     done
 done
 date
--- newone/test.sh
+++ newone/test.sh
@@ -2,8 +2,8 @@ num=$1
 key=$2
 user=$3
 pswd=$4
-peer network login ${user} -p ${pswd}
-#curl -X POST -H 'Content-Type:application/json' --data "{\"enrollId\": \"${user}\",\"enrollSecret\": \"${pswd}\"}" localhost:7050/registrar
+#peer network login ${user} -p ${pswd}
+curl -X POST -H 'Content-Type:application/json' --data "{\"enrollId\": \"${user}\",\"enrollSecret\": \"${pswd}\"}" localhost:8050/registrar
 for i in `seq ${num}` 
 do
   ./create.sh 1000 1 1 ${i}_cp_${key} ${user}> tmp${i}${key}.log 2>&1 &
