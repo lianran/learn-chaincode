91d029f6f4fefa7a1fe2c48a0a957619adcf4cbf
lianran <lianran@yeah.net>
2016-12-21 15:04:10 +0800

    add more node to script

--- newone/create.sh
+++ newone/create.sh
@@ -2,7 +2,9 @@ num=$1 #the num of txs every round
 waittime=$2 # the delay of every tx
 round=$3 #the round num
 key=$4 #the key for this test
-user=$5
+user=$5 #the user for login
+ip=$6 # server's ip
+port=$7 # server's port
 source /etc/profile
 echo "one round will invoke $num txs"
 echo "every tx will wait $waittime s"
@@ -11,12 +13,12 @@ echo "the key for this test is $key"
 unixtime=$(date +%s)
 id=0
 date
-CC_ID="db4fa76606a419673dfc30c929b3950f37c9525ce9739d510b4d4e6529a1986e898435ce5cb36725dd48eb9b48fa8d9b5b18417070c1079db9c8045698ff760c"
+CC_ID="a2e1750d8a559eb9934ba40034242cb23b011cf6a26e4144f1daea7ac563c85acb5eab9dd0ed553031a960568c5f8103baa9328b5af90d4a5fab792b06e45d8e"
 for ((i=0; i < $round; i++)); do
     echo "round $i"
     for ((j=0; j < $num; j++)); do
         #peer chaincode invoke -n ${CC_ID} -c "{\"Args\": [\"create\", \"cp_${i}_${key}_${j}\",\"JD\", \"PKU\", \"${unixtime}\", \"price:${i}${j}\"]}"  -u ${user} &
-        curl -X POST -H 'Content-Type:application/json' --data "{\"jsonrpc\":\"2.0\",\"method\":\"invoke\",\"params\":{\"type\":1,\"chaincodeID\":{\"name\":\"${CC_ID}\"},\"ctorMsg\":{\"args\":[\"create\",\"cp_${i}_${key}_${j}\",\"JD\",\"PKU\",\"${unixtime}\",\"price:${i}${j}\"]},\"secureContext\":\"${user}\"},\"id\":${id}}" localhost:7050/chaincode
+        curl -X POST -H 'Content-Type:application/json' --data "{\"jsonrpc\":\"2.0\",\"method\":\"invoke\",\"params\":{\"type\":1,\"chaincodeID\":{\"name\":\"${CC_ID}\"},\"ctorMsg\":{\"args\":[\"create\",\"cp_${i}_${key}_${j}\",\"JD\",\"PKU\",\"${unixtime}\",\"price:${i}${j}\"]},\"secureContext\":\"${user}\"},\"id\":${id}}" ${ip}:${port}/chaincode
         #echo "the key is : ${key}${i}${j}"
     #sleep $waittime
     id++
--- newone/test.sh
+++ newone/test.sh
@@ -1,10 +1,34 @@
 num=$1
 key=$2
-user=$3
-pswd=$4
-#peer network login ${user} -p ${pswd}
-curl -X POST -H 'Content-Type:application/json' --data "{\"enrollId\": \"${user}\",\"enrollSecret\": \"${pswd}\"}" localhost:8050/registrar
+#node 1
+ip="10.0.2.53"
+port="8050"
+user="admin"
+pswd="Xurw3yU9zI0l"
+curl -X POST -H 'Content-Type:application/json' --data "{\"enrollId\": \"${user}\",\"enrollSecret\": \"${pswd}\"}" ${ip}:${port}/registrar
 for i in `seq ${num}` 
 do
-  ./create.sh 1000 1 1 ${i}_cp_${key} ${user}> tmp${i}${key}.log 2>&1 &
+  ./create.sh 1000 1 1 ${i}_cp_${key}_${user} ${user} ${ip} ${port}> tmp${i}${key}.log 2>&1 &
+done 
+
+#node 2
+ip="10.0.2.53"
+port="9050"
+user="binhn"
+pswd="7avZQLwcUe9q"
+curl -X POST -H 'Content-Type:application/json' --data "{\"enrollId\": \"${user}\",\"enrollSecret\": \"${pswd}\"}" ${ip}:${port}/registrar
+for i in `seq ${num}` 
+do
+  ./create.sh 1000 1 1 ${i}_cp_${key}_${user} ${user} ${ip} ${port}> tmp${i}${key}.log 2>&1 &
+done 
+
+#node 3
+ip="10.0.2.53"
+port="10050"
+user="diego"
+pswd="DRJ23pEQl16a"
+curl -X POST -H 'Content-Type:application/json' --data "{\"enrollId\": \"${user}\",\"enrollSecret\": \"${pswd}\"}" ${ip}:${port}/registrar
+for i in `seq ${num}` 
+do
+  ./create.sh 1000 1 1 ${i}_cp_${key}_${user} ${user} ${ip} ${port}> tmp${i}${key}.log 2>&1 &
 done 
